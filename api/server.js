4a40d337c1fd2be0a6a1bd23da93cd0cde5108f7
Aanand Patel <aanandp2003ca@gmail.com>
2024-12-02 15:48:06 -0600

    hi

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "This is the bew latest" });
+  res.json({ message: "HIIIIIIIII-DISC ON TOP" });
 });
 
 const PORT = process.env.PORT || 3005;
